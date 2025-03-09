c3e9de979761e728e2466a710c354044c8b264b7
Gary Henderson <gch0029@auburn.edu>
2025-03-09 12:42:43 -0500

    added exception handling

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 prompt = "Type add, show, edit, complete or exit: "
+inv_cmd = "Invalid Command."
 while True:
     user_input = input(prompt)
     user_input = user_input.strip()
@@ -25,29 +26,37 @@ while True:
 
     # Mark an item as complete, removes from to do list
     elif user_input.startswith("complete"):
-        with open("to-do.txt", "r") as file:
-            items = file.readlines()
-        cmp_item = int(user_input[9:]) - 1
-        todo_completed = items[cmp_item].strip("\n")
-        items.pop(cmp_item)
-        with open("to-do.txt", "w") as file:
-            file.writelines(items)
-        print(f"Item {todo_completed} removed from list.")
+        try:
+            with open("to-do.txt", "r") as file:
+                items = file.readlines()
+            cmp_item = int(user_input[9:]) - 1
+            todo_completed = items[cmp_item].strip("\n")
+            items.pop(cmp_item)
+            with open("to-do.txt", "w") as file:
+                file.writelines(items)
+            print(f"Item {todo_completed} removed from list.")
+        except ValueError:
+            print(inv_cmd)
+            continue
 
     # Edit an existing item in the to do list
     elif user_input.startswith("edit"):
-        with open("to-do.txt", "r") as file:
-            items = file.readlines()
-        edit_num = int(user_input[5:])
-        edit_num = int(edit_num)
-        if edit_num < 1 or edit_num > len(items):
-            print("Invalid Index")
-        else:
-            edit_item = input("Edit the item: ")
-            items[edit_num-1] = edit_item + '\n'
-        with open("to-do.txt", "w") as file:
-            file.writelines(items)
-        print("Item edited")
+        try:
+            with open("to-do.txt", "r") as file:
+                items = file.readlines()
+            edit_num = int(user_input[5:])
+            edit_num = int(edit_num)
+            if edit_num < 1 or edit_num > len(items):
+                print("Invalid Index")
+            else:
+                edit_item = input("Edit the item: ")
+                items[edit_num-1] = edit_item + '\n'
+            with open("to-do.txt", "w") as file:
+                file.writelines(items)
+            print("Item edited")
+        except ValueError:
+            print(inv_cmd)
+            continue
 
     # Exit the application
     elif user_input == "exit":
@@ -55,4 +64,4 @@ while True:
         break
 
     else:
-        print("Invalid commands")
+        print(inv_cmd)
