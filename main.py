21f6806698ed32a36a7399c4a4cfbe6eb265197e
Gary Henderson <gch0029@auburn.edu>
2025-03-18 18:06:53 -0500

    Added default arguments to functions

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 prompt = "Type add, show, edit, complete or exit: "
 inv_cmd = "Invalid Command."
+filepath = "to-do.txt"
 
 
-def get_items(filepath_in):
+def get_items(filepath_in=filepath):
     with open(filepath_in, "r") as read_file:
         return_items = read_file.readlines()
     return return_items
 
 
-def write_items(filepath_in, items_in):
-    with open("to-do.txt", "w") as write_file:
+def write_items(items_in, filepath_in=filepath):
+    with open(filepath_in, "w") as write_file:
         write_file.writelines(items_in)
 
 
-filepath = "to-do.txt"
 while True:
     user_input = input(prompt)
     user_input = user_input.strip()
@@ -24,12 +24,12 @@ while True:
         add_item = user_input[4:] + "\n"
         items = get_items(filepath)
         items.append(add_item.capitalize())
-        write_items(filepath, items)
+        write_items(items)
         print("Item added")
 
     # Show items in the to do list
     elif "show" in user_input:
-        items = get_items(filepath)
+        items = get_items()
         # new_items = [item.strip("\n") for item in items]
         for i, item in enumerate(items):
             item = item.strip("\n")
@@ -38,11 +38,11 @@ while True:
     # Mark an item as complete, removes from to do list
     elif user_input.startswith("complete"):
         try:
-            items = get_items(filepath)
+            items = get_items()
             cmp_item = int(user_input[9:]) - 1
             todo_completed = items[cmp_item].strip("\n")
             items.pop(cmp_item)
-            write_items(filepath, items)
+            write_items(item)
             print(f"Item {todo_completed} removed from list.")
         except ValueError:
             print(inv_cmd)
@@ -51,7 +51,7 @@ while True:
     # Edit an existing item in the to do list
     elif user_input.startswith("edit"):
         try:
-            items = get_items(filepath)
+            items = get_items()
             edit_num = int(user_input[5:])
             edit_num = int(edit_num)
             if edit_num < 1 or edit_num > len(items):
@@ -59,7 +59,7 @@ while True:
             else:
                 edit_item = input("Edit the item: ")
                 items[edit_num-1] = edit_item + '\n'
-            write_items(filepath, items)
+            write_items(items)
             print("Item edited")
         except ValueError:
             print(inv_cmd)
