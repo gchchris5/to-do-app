0550af4f423104ea3be79edc67974b4afe43c8da
Gary Henderson <gch0029@auburn.edu>
2025-03-18 20:23:54 -0500

    made constant variable

--- functions.py
+++ functions.py
@@ -1,14 +1,15 @@
 import time
-filepath = "to-do.txt"
+
+FILEPATH = "to-do.txt"
 
 
-def get_items(filepath_in=filepath):
+def get_items(filepath_in=FILEPATH):
     with open(filepath_in, "r") as read_file:
         return_items = read_file.readlines()
     return return_items
 
 
-def write_items(items_in, filepath_in=filepath):
+def write_items(items_in, filepath_in=FILEPATH):
     with open(filepath_in, "w") as write_file:
         write_file.writelines(items_in)
 
