f46a6a0091518d52074f73a6f57b5449ac84a304
Gary Henderson <gch0029@auburn.edu>
2025-03-18 20:02:41 -0500

    made function

--- functions.py
+++ functions.py
@@ -1,3 +1,4 @@
+import time
 filepath = "to-do.txt"
 
 
@@ -10,3 +11,14 @@ def get_items(filepath_in=filepath):
 def write_items(items_in, filepath_in=filepath):
     with open(filepath_in, "w") as write_file:
         write_file.writelines(items_in)
+
+
+def welcome_message():
+    now_date = time.strftime("%A, %B %d, %Y")
+    now_time = time.strftime("%I:%M:%S %p")
+    print(f"Welcome, the date is {now_date}. The time is {now_time}")
+
+
+if __name__ == "__main__":
+    welcome_message()
+
