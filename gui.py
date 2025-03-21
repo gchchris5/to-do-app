15e7da04b3fc8f77e596bb372fc21d3063c9dc55
Gary Henderson <gch0029@auburn.edu>
2025-03-21 17:10:40 -0500

    added edit and comlete buttons

--- gui.py
+++ gui.py
@@ -1,24 +1,49 @@
 import FreeSimpleGUI as sg
 import functions
 
-label = sg.Text("Type in an item")
-input_box = sg.InputText(tooltip="Enter item", key="item")
+label = sg.Text("Manage Your To-do List")
+input_box = sg.InputText(tooltip="Enter item", key="new_item")
 add_button = sg.Button("Add")
 exit_button = sg.Button("Exit")
-
+edit_button = sg.Button("Edit")
+complete_button = sg.Button("Complete")
+list_box = sg.Listbox(values=functions.get_items(), key="list_item",
+                      enable_events=True, size=[45, 10])
 layout = [[label],
           [input_box, add_button],
+          [edit_button, complete_button],
+          [list_box],
           [exit_button]]
 window = sg.Window(title="To-do App",
                    layout=layout,
                    font=("Helvetica", 20))
 while True:
     event, values = window.read()
+    print(event)
+    print(values)
     if event == "Add":
         items = functions.get_items()
-        new_item = values["item"] + "\n"
-        items.append(new_item)
+        new_item = values["new_item"] + "\n"
+        items.append(new_item.capitalize())
         functions.write_items(items_in=items)
+        window["list_item"].update(values=items)
+    elif event == "Edit":
+        item_edit = values["list_item"][0]
+        new_item = values["new_item"] + "\n"
+        items = functions.get_items()
+        index = items.index(item_edit)
+        items[index] = new_item.capitalize()
+        functions.write_items(items)
+        window["list_item"].update(values=items)
+    elif event == "list_item":
+        window["new_item"].update(value=values["list_item"][0].strip("\n"))
+    elif event == "Complete":
+        item_complete = values["list_item"][0]
+        items = functions.get_items()
+        index = items.index(item_complete)
+        items.pop(index)
+        functions.write_items(items_in=items)
+        window["list_item"].update(values=items)
     elif event == "Exit" or sg.WIN_CLOSED:
         break
 
