fbfb892c0f764b3df928f2440815ccc95f70f1c6
Gary Henderson <gch0029@auburn.edu>
2025-03-21 15:03:30 -0500

    created, started making UI window

--- gui.py
+++ gui.py
@@ -2,9 +2,10 @@ import FreeSimpleGUI as sg
 
 label = sg.Text("Type in an item")
 input_box = sg.InputText(tooltip="Enter item")
+add_button = sg.Button("Add")
 
-layout = [label, input_box]
-window = sg.Window("To-do App", layout=[layout])
+layout = [[label], [input_box, add_button]]
+window = sg.Window("To-do App", layout=layout)
 window.read()
 window.close()
 
